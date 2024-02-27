a303ce8acb8227acf37afc8688d3567b5596a196
TomasOvalle <144947433+TomasOvalle@users.noreply.github.com>
2024-02-27 17:07:04 -0300

    00 intro (#1)
    Co-authored-by: Tomás <tomasovallec[200~tomasovallec>

---
+++ 00_intro/actividad1.js
@@ -0,0 +1,15 @@
+const nombre = "Dennisse";
+let edad = 23;
+const precio = 1;
+const nombreDeserie = "Pokemon";
+const Peliculas = ["Deadpool", "Batman"];
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(nombreDeserie);
+console.log(Peliculas);
+
+
+edad = edad + 1
+console.log(edad)
---
+++ 00_intro/index.js
@@ -0,0 +1,30 @@
+console.log("hola");
+
+
+/* string */
+const texto = "con camelCase, no con PascalCase"
+console.log(texto);
+
+/* número */ 
+let numeros = 1234
+console.log(numeros);
+
+/* boolean */
+const verdadero = true
+console.log(verdadero);
+
+/* objetos */ 
+const datos = {texto: texto, numeros: numeros, verdadero: verdadero}
+console.log(datos);
+
+/* Array */
+const arreglo = [ 1, "dos", datos, verdadero]
+console.log(arreglo);
+
+numeros = 1
+console.log(numeros);
+
+/* agregar un nuevo valor al arreglo datos */ 
+
+datos.fechadeNacimiento = 1990
+console.log(datos);
